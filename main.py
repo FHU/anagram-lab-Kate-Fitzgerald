d2d914f809dfc7fd865894fa99dc405253841bf4
Kate-Fitzgerald <kate.fitzgerald@students.fhu.edu>
2024-02-09 13:08:25 -0600

    update main.py
    fixed white space problem after made into list

--- main.py
+++ main.py
@@ -1,8 +1,33 @@
 #REMOVE PASS AND FIX THIS FUNCTION
-def anagram():
-    pass
+def anagram(input1,input2):
+    input1 = input1.upper().strip()
+    input1_split = list(input1)
+    
+    for i in input1_split:
+        if i == ' ':
+            input1_split.remove(i)
+        else:
+            continue
+ 
+    input2 = input2.upper().strip()
+    input2_split = list(input2)
+
+    for i in input2_split:
+        if i == ' ':
+            input2_split.remove(i)
+        else: continue
+
+    for i in input1_split:
+        if i in input2_split:
+            continue
+        else:
+            return False
+    return True
+
 
 if __name__ == '__main__':
     #REMOVE PASS YOUR CODE GOES HERE
-    pass
+    input1 = input()
+    input2 = input()
+    print(anagram(input1,input2))
     
